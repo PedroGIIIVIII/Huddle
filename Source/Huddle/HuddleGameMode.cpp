17be44f4b2e79661a947bb6116807da56bbfcc15
RicardoSantos05 <ricardogun10@gmail.com>
2025-04-23 19:31:30 +0100

    Adição mapa Tiago!
    Cena do Tiago (Blockout!)

--- Source/Huddle/HuddleGameMode.cpp
+++ Source/Huddle/HuddleGameMode.cpp
@@ -7,7 +7,7 @@
 AHuddleGameMode::AHuddleGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_FirstPersonCharacter2"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
